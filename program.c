7df4d69c36ad53534287b4e06dea29bebcc1a3aa
Kevin Martin Jose <youcancallmekevin@gmail.com>
2014-06-17 17:16:40 +0530

    Added addition function

--- program.c
+++ program.c
@@ -1,13 +1,15 @@
-#include <strdio.h>
+#include <stdio.h>
 
 void function1();
 void function2();
+void add();
 
 int main()
 {
 	printf("Test program1\n");
 	printf("Teating:...\n");
 	function1();
+	add();
 }
 
 void function1()
@@ -18,4 +20,14 @@ void function1()
 void function2()
 {
 	printf("Another useless funciton\n");
+}
+
+void add()
+{
+	int a;
+	int b;
+	a = 3;
+	b = 4;
+
+	printf("The sum is %d\n",a+b);
 }
